75fa495a2391278980f26222eeba1ed089a07ddb
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-08-26 13:54:56 +0100

    Added ApiHelper class

---
+++ YTSApiClient/ApiHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTSApiClient
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitClient(string token)
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://yts.mx/api/v2/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            ApiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
